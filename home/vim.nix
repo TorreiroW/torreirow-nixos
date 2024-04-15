78d8882427a41e0ef7c03a6da268f273e8d0a81a
TorreiroW <torreirow@dutchyland.net>
2024-04-15 10:24:39 +0200

    configure vim

--- home/vim.nix
+++ home/vim.nix
@@ -2,8 +2,15 @@
 
 programs.vim.plugins = [
   pkgs.vimPlugins.nerdtree
+  pkgs.vimPlugins.fzf-vim
+  pkgs.vimPlugins.vim-terraform
+  pkgs.vimPlugins.vim-nix
 ]; 
 
 programs.vim.enable = true;
+programs.vim.extraConfig = ''
+nmap ,, :NERDTreeFind <CR>
+'';
+
 
 }
