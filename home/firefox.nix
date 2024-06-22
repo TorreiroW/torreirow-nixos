51d8ff320b6c8c42f34914abf0d825d66a271b62
TorreiroW <githubwto@toorren.net>
2024-06-22 16:41:41 +0200

    add firefox to home-manager

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,81 +1,33 @@
-{ config, pkgs, ... }:
-
+{pkgs, ... }:
   let
-    lock-false = {
-      Value = false;
-      Status = "locked";
-    };
-    lock-true = {
-      Value = true;
-      Status = "locked";
-    };
+    addons = pkgs.nur.repos.rycee.firefox-addons;
   in
-{
-  programs = {
-    firefox = {
-      enable = true;
-      package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-        extraPolicies = {
-          DisableTelemetry = true;
-          # add policies here...
-
-          /* ---- EXTENSIONS ---- */
-          ExtensionSettings = {
-            "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-            # uBlock Origin:
-#            "uBlock0@raymondhill.net" = {
-#              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-#              installation_mode = "force_installed";
-#            };
-            # add extensions here...
-            "bitwarden@toorren.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4305759/bitwarden_password_manager-2024.6.2.xpi";
-            installation_mode = "force_installed";
-            };
-          };
-
-          /* ---- PREFERENCES ---- */
-          # Set preferences shared by all profiles.
-          Preferences = {
-            "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-            "extensions.pocket.enabled" = lock-false;
-            "extensions.screenshots.disabled" = lock-true;
-            # add global preferences here...
+  {
+     programs.firefox.enable = true;
+     programs.firefox.policies = {
+      ExtensionSettings = with builtins;
+        let extension = shortId: uuid: {
+          name = uuid;
+          value = {
+            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+            installation_mode = "normal_installed";
           };
         };
+        in listToAttrs [
+          (extension "clear-cache" "clearcache@michel.de.almeida")
+          (extension "ublock-origin" "uBlock0@raymondhill.net")
+          (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+          (extension "aws-role-switch" "{31f7b254-7ac9-4f3a-ae3c-ef67ea153e4a}")
+          (extension "libredirect" "7esoorv3@alefvanoon.anonaddy.me")
+          (extension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}")
+        ];
+        # To add additional extensions, find it on addons.mozilla.org, find
+        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+        # Then, download the XPI by filling it in to the install_url template, unzip it,
+        # run `jq .browser_specific_settings.gecko.id manifest.json` or
+        # `jq .applications.gecko.id manifest.json` to get the UUID
       };
 
-      /* ---- PROFILES ---- */
-      # Switch profiles via about:profiles page.
-      # For options that are available in Home-Manager see
-      # https://nix-community.github.io/home-manager/options.html#opt-programs.firefox.profiles
-      profiles ={
-        profile_0 = {           # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
-          id = 0;               # 0 is the default profile; see also option "isDefault"
-          name = "profile_0";   # name as listed in about:profiles
-          isDefault = true;     # can be omitted; true if profile ID is 0
-          settings = {          # specify profile-specific preferences here; check about:config for options
-            "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
-            "browser.startup.homepage" = "https://nixos.org";
-            "browser.newtabpage.pinned" = [{
-              title = "NixOS";
-              url = "https://nixos.org";
-            }];
-            # add preferences for profile_0 here...
-          };
-        };
-        profile_1 = {
-          id = 1;
-          name = "wtoorren_profile";
-          isDefault = false;
-          settings = {
-            "browser.newtabpage.activity-stream.feeds.section.highlights" = true;
-            "browser.startup.homepage" = "https://ecosia.org";
-            # add preferences for profile_1 here...
-          };
-        };
-      # add profiles here...
-      };
-    };
-  };
-}
+
+  }
+
