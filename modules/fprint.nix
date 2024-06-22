f5261717f7d9be8662f548cf29e153bbd8219e3f
TorreiroW <githubwto@toorren.net>
2024-06-22 17:55:36 +0200

    fix fingerprint setup

--- modules/fprint.nix
+++ modules/fprint.nix
@@ -5,11 +5,12 @@
    fprintd
  ];
 
-services.fprintd.enable = true;
+ services.fprintd.enable = true;
 
-services.fprintd.tod.enable = true;
 
-services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; (If the vfs0090 Driver does not work, use the following driver)
+#services.fprintd.tod.enable = true;
+
+#services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; #(If the vfs0090 Driver does not work, use the following driver)
 
 #services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; (On my device it only worked with this driver)
 
