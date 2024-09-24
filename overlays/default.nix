b8a88bdcbbc20c66cdac20daab84c4dce69b39b7
TorreiroW <githubwto@toorren.net>
2024-09-24 10:12:59 +0200

    remove obsolete overlay; now installed as pkgs/flake

--- overlays/default.nix
+++ overlays/default.nix
@@ -20,16 +20,16 @@ final: prev: {
     });
   };
 
-jsonify-aws-dotfiles = prev.buildGoModule rec {
-    pname = "jsonify-aws-dotfiles";
-    version = "4c60e320b23ee0fae085cfce0b13d3753e39e73e";
-    src = prev.fetchgit {
-      url = "https://github.com/wearetechnative/jsonify-aws-dotfiles.git";
-      rev = "${version}";
-      hash = "sha256-sL1kpWyAVLxoQRJa+m7XSIaM0kxhmE1kOLpnTZVQwB0=";
-    };
-    vendorHash = "sha256-W6XVd68MS0ungMgam8jefYMVhyiN6/DB+bliFzs2rdk=";
-    nativeCheckInputs = with prev; [ less ];
-  };
+#jsonify-aws-dotfiles = prev.buildGoModule rec {
+#    pname = "jsonify-aws-dotfiles";
+#    version = "4c60e320b23ee0fae085cfce0b13d3753e39e73e";
+#    src = prev.fetchgit {
+#      url = "https://github.com/wearetechnative/jsonify-aws-dotfiles.git";
+#      rev = "${version}";
+#      hash = "sha256-sL1kpWyAVLxoQRJa+m7XSIaM0kxhmE1kOLpnTZVQwB0=";
+#    };
+#    vendorHash = "sha256-W6XVd68MS0ungMgam8jefYMVhyiN6/DB+bliFzs2rdk=";
+#    nativeCheckInputs = with prev; [ less ];
+#  };
 }
 
